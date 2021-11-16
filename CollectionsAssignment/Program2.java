0d9305497b2d840bcbf8e4993bd1d1991046ffb5
sambasivaerala <93128562+sambasivaerala@users.noreply.github.com>
2021-11-16 22:11:30 +0530

    Collections Assignment

---
+++ CollectionsAssignment/Program2.java
@@ -0,0 +1,26 @@
+package CollectionsAssignment;
+import java.util.HashSet;
+public class Program2 {
+	public static void main(String args[]) {
+	      /**HashSet declaration**/
+	      HashSet<String> hset = 
+	               new HashSet<String>();
+
+	      /**Adding elements to the HashSet**/
+	      hset.add("Apple");
+	      hset.add("Mango");
+	      hset.add("Grapes");
+	      hset.add("Orange");
+	      hset.add("Fig");
+	      hset.add("DragonFruit");
+	      hset.add("JackFruit");
+	      hset.add("Banana");
+	      hset.add("PineApple");
+	      hset.add("Guava");
+	      /**Addition of duplicate elements**/
+	      hset.add("Apple");
+	      hset.add("Mango");
+	      /**Displaying HashSet elements**/
+	      System.out.println(hset);
+	    }
+}
