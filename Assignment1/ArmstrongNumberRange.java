b715cbef18435baac68dc9d58bfe809073a3db95
sambasivaerala <93128562+sambasivaerala@users.noreply.github.com>
2021-11-16 22:10:13 +0530

    Basic Data Structure Assignment

---
+++ Assignment1/ArmstrongNumberRange.java
@@ -0,0 +1,64 @@
+package Assignment1;
+import java.util.*;
+public class ArmstrongNumberRange {
+	static void checkArmstrongRange()
+	
+	{
+			
+	int i;
+			
+	for(i=101;i<1000;i++)
+			
+	{
+
+	int count=0,rem=0,temp=i;
+				
+	double x=0.0;
+				
+	while(temp!=0)
+				
+	{
+					
+	temp=temp/10;
+					
+	count=count+1;
+				
+	}
+				
+	
+				
+	temp=i;
+				
+	while(temp!=0)
+				
+	{
+					
+	rem=temp%10;
+					
+	x= x + Math.pow(rem,count);
+					
+	temp=temp/10;
+				
+	}
+				
+	if(x==i)
+				
+	{
+
+	System.out.println("Given number " + i + " is an armstrong number");
+				
+	}
+			
+	}
+		
+	}
+		
+	public static void main(String[] args) 
+		
+	{
+			
+	checkArmstrongRange();
+		
+	}
+
+}
