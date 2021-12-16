006cb78498affe53ab07d926c14c7b8ef81ee648
sambasivaerala <93128562+sambasivaerala@users.noreply.github.com>
2021-12-16 16:12:33 +0530

    Spring Rest Assignments

---
+++ Spring Rest Assignment/SpringRestEmployeeManagementSystem/src/test/java/SpringRestEmployeeManagementSystem/SpringRestEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringRestEmployeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestEmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
